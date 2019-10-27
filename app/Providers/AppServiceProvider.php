07cfb87eb40fd1fac1f5249c7f8b6bc6f3e41bb7
Faruk Nasir <faruk@Faruks-MacBook-Pro.local>
2019-10-27 23:11:07 +0100

    indicate public folder in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Foundation\Application;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(App::environment('production')) {
+			$this->app->bind('path.public', function() {
+				return base_path('public');
+			});
+		}
     }
 
     /**
